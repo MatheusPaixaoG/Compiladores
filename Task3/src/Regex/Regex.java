c60c9320ba2e2e03cdc0d9d73ab278c438cc38f1
Matheus Paixão <mpgs@cin.ufpe.br>
2023-04-27 20:52:05 -0300

    Added methods to check if token is num or operation

--- Task3/src/Regex/Regex.java
+++ Task3/src/Regex/Regex.java
@@ -10,16 +10,30 @@ public class Regex {
     private String REGEX_SLASH = "(/)";
     private String REGEX_POW = "(\\^)";
 
-    public TokenType getTokenType(String token) {
+    private boolean isNum(String token) {
+        return token.matches(REGEX_NUM);
+    }
+
+    private boolean isOP(String token) {
+        return token.matches("(\\+|\\-|\\*|\\^|/)");
+    }
+
+    private TokenType determineOperation(String token) {
         if (token.matches(REGEX_PLUS)) {
             return TokenType.PLUS;
         } else if (token.matches(REGEX_MINUS)) {
             return TokenType.MINUS;
         } else if (token.matches(REGEX_STAR)) {
             return TokenType.STAR;
-        } else if (token.matches(REGEX_SLASH)) {
+        } else {
             return TokenType.SLASH;
-        } else if (token.matches(REGEX_NUM)) {
+        }
+    }
+
+    public TokenType getTokenType(String token) {
+        if (this.isOP(token)) {
+            return determineOperation(token);
+        } else if (this.isNum(token)) {
             return TokenType.NUM;
         } else {
             return TokenType.NOTVALID;
